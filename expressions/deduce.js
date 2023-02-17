750bd6dbf03583944e931ad7710a4a5b806cb013
Sankar Ganesh <sankar.ganesh@freshworks.com>
2023-02-17 11:28:47 +0530

    test one

--- expressions/deduce.js
+++ expressions/deduce.js
@@ -1,4 +1,24 @@
+function apply_expressions(x = 1, y = 1, z = 1) {
+    if (x === 1) {
+        return (x * y) + (x * z);
+    }
+    if (y === 1) {
+        return (x + y) * (y + z);
+    }
+    if (z === 1) {
+        return (x + z) / (y + z);
+    }
+}
+
 (function Deduce() {
+    const x = 1, y = 2, z = 1;
     console.log('start deduce');
+    console.log(apply_expressions(x, y, z));
+    console.log(apply_expressions(x, y));
+    console.log(apply_expressions(x, z));
+    console.log(apply_expressions(x));
+    console.log(apply_expressions(y));
+    console.log(apply_expressions(y, z));
+    console.log(apply_expressions(z));
     console.log('end deduce');
 })();
